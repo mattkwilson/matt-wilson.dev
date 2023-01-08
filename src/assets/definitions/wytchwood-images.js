fa55659aff332fe3141a66d7dcb627dd08361cf4
Matt <matt.wilson720@gmail.com>
2023-01-08 12:50:42 -0800

    Switch Youtube links with local videos

---
+++ src/assets/definitions/wytchwood-images.js
@@ -0,0 +1,10 @@
+import wytchwoodImage1 from '../images/wytchwood/wytchwood-screenshot-1.jpg';
+import wytchwoodImage2 from '../images/wytchwood/wytchwood-screenshot-2.jpg';
+import wytchwoodImage3 from '../images/wytchwood/wytchwood-screenshot-3.jpg';
+import wytchwoodImage4 from '../images/wytchwood/wytchwood-screenshot-4.jpg';
+import wytchwoodImage5 from '../images/wytchwood/wytchwood-screenshot-5.jpg';
+
+const images = [wytchwoodImage1, wytchwoodImage2, wytchwoodImage3, wytchwoodImage4, wytchwoodImage5];
+
+export default images; 
+
